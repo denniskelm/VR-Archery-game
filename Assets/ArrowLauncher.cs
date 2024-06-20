5e22d591c42028a84323f493a506f42225b0546b
Dennis Kelm <email@denniskelm.de>
2024-06-20 20:35:38 +0200

    Added arrow physics

--- Assets/ArrowLauncher.cs
+++ Assets/ArrowLauncher.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class ArrowLauncher : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject ArrowPrefab;
+    
     void Start()
+    {
+        InvokeRepeating("launchArrow", 1.0f, 0.3f);
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void launchArrow()
     {
+        GameObject arrow = ArrowPrefab;
+        arrow.transform.position = new Vector3(1, 2, 3);
         
+
+        GameObject instance = Instantiate(arrow);
+        instance.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-2f, 2f), 1f, Random.Range(2f, 5f));
     }
 }
