ed1b62c9d62e158eb98956fae7653a8b83855f1a
Dennis Kelm <email@denniskelm.de>
2024-06-20 21:19:13 +0200

    Minor arrow launcher updates

--- Assets/ArrowLauncher.cs
+++ Assets/ArrowLauncher.cs
@@ -6,15 +6,17 @@ public class ArrowLauncher : MonoBehaviour
 {
 
     public GameObject ArrowPrefab;
+
+    private GameObject instance;
     
     void Start()
     {
         InvokeRepeating("launchArrow", 1.0f, 0.3f);
     }
 
-    void Update()
-    {
-        
+    void FixedUpdate(){
+        if(instance != null && instance.GetComponent<Rigidbody>().velocity != Vector3.zero)
+            instance.GetComponent<Rigidbody>().rotation = Quaternion.LookRotation(instance.GetComponent<Rigidbody>().velocity);  
     }
 
     public void launchArrow()
@@ -23,7 +25,10 @@ public class ArrowLauncher : MonoBehaviour
         arrow.transform.position = new Vector3(1, 2, 3);
         
 
-        GameObject instance = Instantiate(arrow);
+        instance = Instantiate(arrow);
+        instance.transform.forward =
+            Vector3.Slerp(instance.transform.forward, instance.GetComponent<Rigidbody>().velocity.normalized, Time.deltaTime);
+
         instance.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-2f, 2f), 1f, Random.Range(2f, 5f));
     }
 }
